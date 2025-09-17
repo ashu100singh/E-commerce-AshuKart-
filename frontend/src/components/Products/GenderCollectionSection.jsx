56a936d5d87fb0911dbb2919b67eb94db81b5fdc
Ashutosh Singh <ashutosh100singh100@gmail.com>
2025-09-17 06:58:44 +0530

    Created GenderCollection, New Arrivals and ProductDetails section on homepage with their functionalitties

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import mensCollectionImage from "../../assets/mens-collection.webp"
 import womensCollectionImage from "../../assets/womens-collection.webp"
+import { Link } from 'react-router-dom'
 
 const GenderCollectionSection = () => {
-  return (
-    <section className='py-16 px-4 lg:px-0'>
-        <div className='container mx-auto flex flex-col md:flex-row gap-8'>
-            <div className=''>
-
-            </div>
-        </div>
-    </section>
-  )
+	return (
+		<section className='py-16 px-4 lg:px-0'>
+			<div className='container mx-auto flex flex-col md:flex-row gap-8'>
+				{/* Women's Collection */}
+				<div className='relative flex-1'>
+					<img
+						src={womensCollectionImage}
+						alt='Womens_collection'
+						className='w-full object-cover h-[700px]'
+					/>
+					<div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+						<h2 className='text-2xl font-bold text-gray-900 mb-3'>
+							Women's Collection
+						</h2>
+						<Link to='/collections/all?gender=Women' className='text-gray-900 underline'>
+							Shop Now
+						</Link>
+					</div>
+				</div>
+				{/* Men's Collection */}
+				<div className='relative flex-1'>
+					<img
+						src={mensCollectionImage}
+						alt='Womens_collection'
+						className='w-full object-cover h-[700px]'
+					/>
+					<div className='absolute bottom-8 left-8 bg-white/90 p-4'>
+						<h2 className='text-2xl font-bold text-gray-900 mb-3'>
+							Men's Collection
+						</h2>
+						<Link to='/collections/all?gender=Men' className='text-gray-900 underline'>
+							Shop Now
+						</Link>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default GenderCollectionSection

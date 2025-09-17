8e76e1aaef31d505365479ecc66390e8d3b76e17
Ashutosh Singh <ashutosh100singh100@gmail.com>
2025-09-17 08:53:00 +0530

    Completed Home Page by adding all other sections

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner'
+import ProductGrids from './ProductGrids'
 
 const selectedProduct = {
     name: "Stylish Jacket",
@@ -21,6 +22,33 @@ const selectedProduct = {
         },
     ]
 }
+
+const similarProducts = [
+    {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=10"}],
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=11"}],
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=12"}],
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=13"}],
+    },
+]
 
 const ProductDetails = () => {
 
@@ -183,10 +211,15 @@ const ProductDetails = () => {
                             </table>
                         </div>
                     </div>
-
-                    {/*  */}
-
                 </div>
+
+                <div className='mt-20'>
+                    <h2 className='text-2xl text-center font-medium mb-4'>
+                        You May Also Like
+                    </h2>
+                    <ProductGrids products={similarProducts}/>
+                </div>
+
             </div>
         </div>
     )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,60 @@ import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
+import ProductGrids from '../components/Products/ProductGrids'
+import FeaturedCollection from '../components/Products/FeaturedCollection'
+import FeaturesSection from '../components/Products/FeaturesSection'
+
+const placeholderProducts = [
+	{
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=14"}],
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=15"}],
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=12"}],
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=16"}],
+    },
+	{
+        _id: 5,
+        name: "Product 5",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=17"}],
+    },
+    {
+        _id: 6,
+        name: "Product 6",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=18"}],
+    },
+    {
+        _id: 7,
+        name: "Product 7",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=19"}],
+    },
+    {
+        _id: 8,
+        name: "Product 8",
+        price: 100,
+        images: [{url: "https://picsum.photos/500/500?random=20"}],
+    },
+]
 
 const Home = () => {
   return (
@@ -16,6 +70,16 @@ const Home = () => {
           Best Seller
         </h2>
         <ProductDetails/>
+
+        <div className='container mx-auto'>
+			<h2 className='text-3xl text-center font-bold mb-4'>
+				Top Wears for Women
+			</h2>
+			<ProductGrids products={placeholderProducts}/>
+        </div>
+
+		<FeaturedCollection/>
+		<FeaturesSection/>
     </div>
   )
 }

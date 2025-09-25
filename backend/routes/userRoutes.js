c9d417df1ea4fb4fe7a8de62a9a296bade2491b0
Ashutosh Singh <ashutosh100singh100@gmail.com>
2025-09-25 07:57:11 +0530

    Added login logic and a protected route for viewing profile only for logged in users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const User = require("../models/User") 
 const jwt = require("jsonwebtoken")
+const { protect } = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
@@ -45,6 +46,62 @@ router.post("/register", async (req, res) => {
         console.log(error)
         res.status(500).send("Server Error")
     }
+})
+
+
+//@route POST api/users/login
+//@desc Authenticate and login user
+//@access PUBLIC
+router.post("/login", async(req, res) => {
+    const { email, password } = req.body
+    try {
+        let user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        const ismatch = await user.matchPassword(password)
+
+        if(!ismatch){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        //JWT Payload
+        const payload = { user: {id: user._id, role: user.role} }
+
+        //Sign and return the token along with user data
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn: "24h"},
+            (err, token) => {
+                if(err) throw err
+
+                res.status(200).json({
+                    user:{
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role
+                    },
+                    token
+                })
+            }
+        )
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({message: "Server Error"})
+    }
+})
+
+
+//@route GET api/users/profile
+//@desc Get loggged In user's profile
+//@access PRIVATE
+router.get("/profile", protect, async(req, res) => {
+    res.json(req.user)
 })
 
 module.exports = router
